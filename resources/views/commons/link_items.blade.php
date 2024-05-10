054aecd90a0ecde735cf38e55cc201f3188a43c5
yu_sato <yu_sato@net-nsi.co.jp>
2024-05-10 07:01:04 +0000

    top page

---
+++ resources/views/commons/link_items.blade.php
@@ -0,0 +1,11 @@
+@if (Auth::check())
+    <li class="divider lg:hidden"></li>
+    {{-- ログアウトへのリンク --}}
+    <li><a class="link link-hover" href="#" onclick="event.preventDefault();this.closest('form').submit();">ログアウト</a></li>
+@else
+    {{-- ユーザ登録ページへのリンク --}}
+    <li><a class="link link-hover" href="{{ route('register') }}">会員登録</a></li>
+    <li class="divider lg:hidden"></li>
+    {{-- ログインページへのリンク --}}
+    <li><a class="link link-hover" href="{{ route('login') }}">ログイン</a></li>
+@endif
